cf689c4763ac9eedc6fc2d8cf3e8029bfdff5603
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-10-22 17:23:48 +0545

    dsfsdf

--- app/Http/Controllers/FlowchartController.php
+++ app/Http/Controllers/FlowchartController.php
@@ -6,5 +6,9 @@ use Illuminate\Http\Request;
 
 class FlowchartController extends Controller
 {
-    //
+    public function listing()
+    {
+        $diagramHtml = '<iframe src="https://draw.io/?<diagram-id>" frameborder="0" width="100%" height="600"></iframe>';
+        return View::make('flowcharts.listing', ['diagramHtml' => $diagramHtml]);
+    }
 }
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,12 @@
 <?php
 
+use App\Http\Controllers\FlowchartController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
+
+Route::prefix('flowchart')->group(function () {
+    Route::get('/listing', [FlowchartController::class, 'listing'])->name("flowchart.listing");
+});
