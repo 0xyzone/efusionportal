fe18fd9d17a450fb23349b6acf093e1b2ffa51e7
0xyzone <58982560+0xyzone@users.noreply.github.com>
2024-10-22 19:37:06 +0545

    Update FlowchartController.php

--- app/Http/Controllers/FlowchartController.php
+++ app/Http/Controllers/FlowchartController.php
@@ -3,12 +3,13 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\View;
 
 class FlowchartController extends Controller
 {
     public function listing()
     {
-        $diagramHtml = '<iframe src="https://draw.io/?<diagram-id>" frameborder="0" width="100%" height="600"></iframe>';
+        $diagramHtml = '<iframe src="https://app.diagrams.net/?embed=1&ui=atlas&chrome=0&title=Diagram&url=https://drive.google.com/uc?export=download&id=1ozGINa1DCSDYzOIo1C8WuZ3Ry8lV0jSb" frameborder="0" width="100%" height="600"></iframe>';
         return View::make('flowcharts.listing', ['diagramHtml' => $diagramHtml]);
     }
 }
